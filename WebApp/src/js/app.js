7092cccb51180b607638da601c993db5cad69d23
nicomoccagatta <nicomoccagatta@gmail.com>
2016-09-06 00:15:55 -0300

    [#2] Layout principal de la app aplicado, e implementacion de un menu desplegable

--- WebApp/src/js/app.js
+++ WebApp/src/js/app.js
@@ -1,1 +1,11 @@
-angular.module('App', ['ngMaterial']) ;
+angular.module('App',['ngMaterial'])
+
+.controller('AppController', function($scope,$mdSidenav) {
+  /**
+   * Hide or Show the 'left' sideNav area
+   */
+  $scope.toggleList = function toggleList() {
+    $mdSidenav('left').toggle();
+  }
+
+});
