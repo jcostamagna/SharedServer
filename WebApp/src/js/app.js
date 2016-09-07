f0953df54a661b31d06757219dba71fae8575f5c
nicomoccagatta <nicomoccagatta@gmail.com>
2016-09-07 14:43:24 -0300

    #2 [TOOLBAR][FAB] Implementados la toolbar que tiene una barra de busqueda, y un fab para agregar elementos a la lista de items

--- WebApp/src/js/app.js
+++ WebApp/src/js/app.js
@@ -1,6 +1,6 @@
-angular.module('App',['ngMaterial'])
+var app = angular.module('App',['ngMaterial', 'ngMdIcons']);
 
-.controller('AppController', function($scope,$mdSidenav) {
+app.controller('AppController', ['$scope','$mdSidenav', '$mdDialog', function($scope,$mdSidenav, $mdDialog) {
   /**
    * Hide or Show the 'left' sideNav area
    */
@@ -8,8 +8,32 @@ angular.module('App',['ngMaterial'])
     $mdSidenav('left').toggle();
   }
 
-	$scope.sizes = [
-        {size:"md-48",padding:10}
-      ];
 
-});
+  $scope.showAdd = function(ev) {
+    $mdDialog.show({
+      controller: DialogController,
+      template: '<md-dialog aria-label="Mango (Fruit)"> <md-content class="md-padding"> <form name="userForm"> <div layout layout-sm="column"> <md-input-container flex> <label>First Name</label> <input ng-model="user.firstName" placeholder="Placeholder text"> </md-input-container> <md-input-container flex> <label>Last Name</label> <input ng-model="theMax"> </md-input-container> </div> <md-input-container flex> <label>Address</label> <input ng-model="user.address"> </md-input-container> <div layout layout-sm="column"> <md-input-container flex> <label>City</label> <input ng-model="user.city"> </md-input-container> <md-input-container flex> <label>State</label> <input ng-model="user.state"> </md-input-container> <md-input-container flex> <label>Postal Code</label> <input ng-model="user.postalCode"> </md-input-container> </div> <md-input-container flex> <label>Biography</label> <textarea ng-model="user.biography" columns="1" md-maxlength="150"></textarea> </md-input-container> </form> </md-content> <div class="md-actions" layout="row"> <span flex></span> <md-button ng-click="answer(\'not useful\')"> Cancel </md-button> <md-button ng-click="answer(\'useful\')" class="md-primary"> Save </md-button> </div></md-dialog>',
+      targetEvent: ev,
+    })
+    .then(function(answer) {
+      $scope.alert = 'You said the information was "' + answer + '".';
+    }, function() {
+      $scope.alert = 'You cancelled the dialog.';
+    });
+  };
+
+}]);
+
+
+function DialogController($scope, $mdDialog) {
+  $scope.hide = function() {
+    $mdDialog.hide();
+  };
+  $scope.cancel = function() {
+    $mdDialog.cancel();
+  };
+  $scope.answer = function(answer) {
+    $mdDialog.hide(answer);
+  };
+};
+
