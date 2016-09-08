f1fe6a7917e919a13104f87163f5849ce75879ac
jcostamagna <j_costamagna@hotmail.com>
2016-09-08 18:17:26 -0300

    Servidor con puerto variable con el entorno. #4

--- Server/server.js
+++ Server/server.js
@@ -20,7 +20,7 @@ function iniciar(route, handle) {
 
   }
 
-  http.createServer(onRequest).listen(8888);
+  http.createServer(onRequest).listen(process.env.PORT || 5000);
   console.log("Servidor Iniciado");
 }
 
