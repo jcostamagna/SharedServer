35ed66b177403e23a56e05408ba46de62530a71e
jcostamagna <j_costamagna@hotmail.com>
2016-09-15 11:41:21 -0300

    Servidor en un solo script simple usando express closes #14, y todos los archivos de mas fueron borrados. Refactorizacion completa closes #13

--- server.js
+++ server.js
@@ -2,24 +2,16 @@
 
 const express = require('express');
 var path = require("path");
+
 // Constants
 const PORT = 8080;
 
 // App
 const app = express();
-//app.get('/', function (req, res) {
-//    res.send('Hello world\n');
-//});
 
 // Configuración
-    // Localización de los ficheros estÃ¡ticos
-    app.use(express.static(path.join(__dirname,'/WebApp')));
-    // Muestra un log de todos los request en la consola        
-   // app.use(express.logger('dev')); 
-    // Permite cambiar el HTML con el método POST                   
-  //  app.use(express.bodyParser());
-    // Simula DELETE y PUT                      
-  //  app.use(express.methodOverride());  
+app.use(express.static(path.join(__dirname,'/WebApp')));
+
 
 app.get('*', function(req, res) {  
     res.sendFile('WebApp/index.html');                
