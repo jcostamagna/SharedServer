0df3ff15147736c61a7d76b1097df01bd4f95cd6
jcostamagna <j_costamagna@hotmail.com>
2016-10-12 23:40:50 -0300

    Pruebas de categoria avanzadas, pasando. Todos los GET,POST y DELETE de categoria funcionando. #3, #15, #16, #17, #19

--- RestAPITest.py
+++ RestAPITest.py
@@ -3,49 +3,90 @@ import unittest
 import requests
 import json
 #~ import responses
-
+#~ URL = 'http://localhost:8080'
+URL = 'https://tallersharedserver.herokuapp.com/'
 
 
 class TestCase(unittest.TestCase):
 
   #@responses.activate  
   def setUp(self):
-
-    response = requests.get('https://tallersharedserver.herokuapp.com/test')
+    response = requests.get(URL + '/test')
     self.assertEqual(json.dumps([]), response.text)
     self.assertEqual(201, response.status_code)
+    self.checkEmptyBD()
     
-  def testCategoryInsertSimple(self):
-    payload = {'category':{'name': 'sport', 'description': 'sport activities'}}
+  def CategoryInsertSimple(self, name, description): 
+    payload = {'category':{'name': name, 'description': description}}
     headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-    response = requests.post('https://tallersharedserver.herokuapp.com/categories', data=json.dumps(payload), headers = headers)
+    response = requests.post(URL + '/categories', data=json.dumps(payload), headers = headers)
     self.assertEqual(json.dumps(payload), json.dumps(json.loads(response.text)))
     self.assertEqual(201, response.status_code)
+    
+  def CategoryDeleteSimple(self, name):
+	response = requests.delete(URL + '/categories/' + name)
+	self.assertEqual(204, response.status_code)
+	
+  def getCategories(self):
+	   return requests.get(URL + '/categories')
     
     
   def checkEmptyBD(self):
 	#Chequear que este vacio
-	response = requests.get('http://localhost:8080/categories')
-	espected = {"categories": [{}],"metadata": {"count": 0}}
+	response = self.getCategories()
+	espected = {"categories": [],"metadata": {"count": 0}}
+
 	self.assertEqual(json.dumps(espected), json.dumps(json.loads(response.text)))
 	self.assertEqual(201, response.status_code)
     
     
-  def CategoryInsertAndDelete(self):
+  def testCategoryInsertAndDelete(self):
 	self.checkEmptyBD()
 	
 	#Agregar categoria 1
-	payload = {'category':{'name': 'sport2', 'description': 'sport activities2'}}
-	headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-	response = requests.post('http://localhost:8080/categories', data=json.dumps(payload), headers = headers)
-	self.assertEqual(json.dumps(payload), response.text)
-	self.assertEqual(201, response.status_code)
+	self.CategoryInsertSimple('sport', 'sport activities')
+	
+	response = self.getCategories()
+	espected = {"categories": [{ "name": 'sport', "description": 'sport activities' }],"metadata": {"count": 1}}
+	self.assertEqual(json.dumps(espected), json.dumps(json.loads(response.text)))
 	
 	#Eliminamos categoria 1
-	response = requests.delete('http://localhost:8080/categories/sport2')
-	self.assertEqual(204, response.status_code)
+	self.CategoryDeleteSimple('sport')
 	
 	self.checkEmptyBD()
+	
+  def testCategoryIntertTwoDeleteTwo(self):
+	self.checkEmptyBD()
+	  
+	#Agregar categoria 1
+	self.CategoryInsertSimple('software', 'software activities')
+	
+	#Chequeo de categorias
+	response = self.getCategories()
+	espected = {"categories": [{ "name": 'software', "description": 'software activities' }],"metadata": {"count": 1}}
+	self.assertEqual(json.dumps(espected), json.dumps(json.loads(response.text)))
+	
+	#Agregar categoria 2
+	self.CategoryInsertSimple('administration', 'administration activities')
+	
+	#Chequeo de categorias
+	response = self.getCategories()
+	espected = {"categories": [{ "name": 'software', "description": 'software activities' },{ "name": 'administration', "description": 'administration activities' }],"metadata": {"count": 2}}
+	self.assertEqual(json.dumps(espected), json.dumps(json.loads(response.text)))
+	
+	#Eliminamos categoria 2
+	self.CategoryDeleteSimple('administration')
+	
+	#Chequeo de categorias
+	response = self.getCategories()
+	espected = {"categories": [{ "name": 'software', "description": 'software activities' }],"metadata": {"count": 1}}
+	self.assertEqual(json.dumps(espected), json.dumps(json.loads(response.text)))
+	
+	#Eliminamos categoria 1
+	self.CategoryDeleteSimple('software')
+	
+	self.checkEmptyBD()
+	
 	
     
 if __name__ == '__main__':
--- server.js
+++ server.js
@@ -14,20 +14,19 @@ const app = express();
 pg.defaults.ssl = true;
 
 
-//app.use(bodyParser());
-
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 })); 
-//~ app.use( bodyParser.json() );       // to support JSON-encoded bodies
-//app.use(bodyParser.urlencoded({ extended: true }))    // parse application/x-www-form-urlencoded
 app.use(bodyParser.json())    // parse application/json
 
-const connectionString = process.env.DATABASE_URL || 'postgres://koicdmjsauxtuc:2juyGj1IcOMLPrbK_vhaBj0v5V@ec2-54-221-225-242.compute-1.amazonaws.com:5432/debpfqvr21od6g';
+//~ var connectionString = process.env.DATABASE_URL || 'postgres://koicdmjsauxtuc:2juyGj1IcOMLPrbK_vhaBj0v5V@ec2-54-221-225-242.compute-1.amazonaws.com:5432/debpfqvr21od6g';
+//~ var connectionString = process.env.DATABASE_URL || 'postgres://juan:12Oct1993@localhost:5432/sharedServer?sslmode=require';
+var connectionString = process.env.DATABASE_URL || 'postgres://uvwiyhoazhndqk:28c0drCwyXJILkCJMxjHz38LDq@ec2-23-23-225-98.compute-1.amazonaws.com:5432/dfp1uurfqcikj2';
 
-const client = new pg.Client(connectionString);
+
+var client = new pg.Client(connectionString);
 client.connect();
-const query = client.query(
+var query = client.query(
   'CREATE TABLE IF NOT EXISTS categorias(nombre VARCHAR(40) PRIMARY KEY, descripcion VARCHAR(140))');
 query.on('end', () => { client.end(); });
 
@@ -36,8 +35,74 @@ query.on('end', () => { client.end(); });
 app.use(express.static(path.join(__dirname,'/WebApp')));
 
 app.get('/test', function(req, res) {
-	const results = [];
-	pg.connect(connectionString, (err, client, done) => {
+	pg.connect(connectionString, function(err, client, done) {
+    // Handle connection errors
+    if(err) {
+      done();
+      console.log(err);
+      return res.status(404).json({success: false, data: err, context: '/test'});
+    }
+    
+    // SQL Query > DELETE Data
+    var query = client.query('DELETE FROM categorias');
+
+    // After all data is returned, close connection and return results
+	query.on('end', function() {
+		done();
+		return res.status(201).json([]);
+	});
+  });
+             
+});
+
+app.get('/', function(req, res) { 
+    res.sendFile('WebApp/index.html');                
+});
+
+
+app.get('/categories', function(req, res, next) {
+	var rows = {};
+	rows['categories'] = [];
+	rows['metadata'] = {};
+	var registro = {};
+	
+	// Get a Postgres client from the connection pool
+	pg.connect(connectionString, function(err, client, done) {
+    // Handle connection errors
+		if(err) {
+			done();
+			console.log(err);
+			return res.status(500).json({success: false, data: err, context: '/categories'});
+		}
+    
+		var length = 0;
+		// SQL Query > Select Data
+		var query = client.query('SELECT * FROM categorias');
+		// Stream results back one row at a time
+		query.on('row', function(row) {
+			registro = {};
+			registro['name'] = row.nombre;
+			registro['description'] = row.descripcion;
+			//console.log(registro);
+			rows['categories'].push(registro);
+			length += 1;
+			//results.push({'name':row.nombre, 'description':row.descripcion}.toObject());
+			//console.log({'name':row.nombre, 'description':row.descripcion});
+		});
+		
+		// After all data is returned, close connection and return results
+		query.on('end', function() {
+			done();
+			rows['metadata']['count'] = length;
+			//console.log(rows);
+			return res.status(201).json(rows);
+		});
+	});
+});
+
+
+app.delete('/categories/:category', function (req, res) {
+	pg.connect(connectionString, function(err, client, done) {
     // Handle connection errors
     if(err) {
       done();
@@ -45,70 +110,26 @@ app.get('/test', function(req, res) {
       return res.status(404).json({success: false, data: err, context: '/test'});
     }
     // SQL Query > DELETE Data
-    client.query('DELETE FROM categorias');
-    // SQL Query > Select Data
-    const query = client.query('SELECT * FROM categorias');
-    // Stream results back one row at a time
-    query.on('row', (row) => {
-      results.push(row);
-    });
+    var query = client.query("DELETE FROM categorias WHERE nombre IN ($1)", [req.params.category]);
+
     // After all data is returned, close connection and return results
-    query.on('end', () => {
-      done();
-      return res.status(201).json(results);
-    });
+	query.on('end', function() {
+		done();
+		return res.status(204).json({});
+	});
+  
+	
   });
-    //res.write('Prueba GET');
-    //res.end();                
+
 });
 
-app.get('*', function(req, res) {  
-    res.sendFile('WebApp/index.html');                
-});
 
-app.get('/categories', (req, res, next) => {
-  const results = [];
+app.post('/categories', function(req, res, next){
   // Grab data from http request
-  // Get a Postgres client from the connection pool
-  pg.connect(connectionString, (err, client, done) => {
-    // Handle connection errors
-    if(err) {
-      done();
-      console.log(err);
-      return res.status(500).json({success: false, data: err, context: '/categories'});
-    }
-    // SQL Query > Select Data
-    const query = client.query('SELECT * FROM categorias');
-    // Stream results back one row at a time
-    query.on('row', (row) => {
-      results.push({'name':row[0], 'description':row[1]});
-    });
-    // After all data is returned, close connection and return results
-    query.on('end', () => {
-      done();
-    });
-  });
-  //res.setHeader('Content-Type', 'application/json');
-  return res.status(201).json({'categories':results, 'metadata': {'count':0}});
-});
-
-
-app.delete('/categories/:category', function (req, res) {
-	console.log(req.params.category)
-  res.send('DELETE request to homepage');
-});
-
-
-app.post('/categories', (req, res, next) => {
-  const results = [];
-  // Grab data from http request
-  //~ console.log(req.body);
-  //~ console.log(req.header);
-  //~ console.log(req.body.category.name);
-  //~ console.log(req.body.category.description);
-  const data = {nombre: req.body.category.name, descripcion: req.body.category.description};
-  // Get a Postgres client from the connection pool
-  pg.connect(connectionString, (err, client, done) => {
+  var data = {nombre: req.body.category.name, descripcion: req.body.category.description};
+  
+  // Get a Postgres client from the connection pool  
+  pg.connect(connectionString, function(err, client, done) {
     // Handle connection errors
     if(err) {
       done();
@@ -116,21 +137,16 @@ app.post('/categories', (req, res, next) => {
       return res.status(500).json({success: false, data: err, context: '/categories'});
     }
     // SQL Query > Insert Data
-    client.query('INSERT INTO categorias(nombre, descripcion) values($1, $2)',
+    var query = client.query('INSERT INTO categorias(nombre, descripcion) values($1, $2)',
     [data.nombre, data.descripcion]);
-    // SQL Query > Select Data
-    /*const query = client.query('SELECT * FROM categorias');
-    // Stream results back one row at a time
-    query.on('row', (row) => {
-      results.push(row);
-    });*/
-    // After all data is returned, close connection and return results
-    query.on('end', () => {
-      done();
-    });
+
+        // After all data is returned, close connection and return results
+	query.on('end', function() {
+		done();
+		return res.status(201).json(req.body);
+	});
   });
-  //res.setHeader('Content-Type', 'application/json');
-  return res.status(201).json(req.body);
+
 });
 
 
